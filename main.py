19d268bab4d591c8343f32c2af9391b18b050256
vetvistiy <154645556+vetvistiy@users.noreply.github.com>
2026-05-06 01:07:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram.filters import CommandStart
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 TOKEN = "8064418763:AAFdApyrYjA_bTguEtWz8aZuqqXI5pk00m8"
 
-TARGET_BOT = "konoplyankabot"
+TARGET_BOT_ID = 7172499939
 
-DELETE_DELAY = 1800
+DELETE_DELAY = 300
+
+YOUR_BOT_USERNAME = "autodeletekonoplyanka_bot"
+
 
 bot = Bot(TOKEN)
 dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start_cmd(message: Message):
+    text = (
+        "🧹 <b>Cleaner Bot</b>\n\n"
+        "Автоматически удаляет сообщения выбранного бота "
+        "каждые 5 минут.\n\n"
+        "Добавь меня в чат и выдай право:\n"
+        "• удаление сообщений"
+    )
+
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="➕ Добавить в чат",
+                    url=f"https://t.me/{autodeletekonoplyanka_bot}?startgroup=true"
+                )
+            ]
+        ]
+    )
+
+    await message.answer(text, reply_markup=kb, parse_mode="HTML")
 
 
 @dp.message()
@@ -17,14 +45,12 @@ async def auto_delete(message: Message):
     if not message.from_user:
         return
 
-    username = message.from_user.username
-
-    if username and username.lower() == TARGET_BOT.lower():
+    if message.from_user.id == TARGET_BOT_ID:
         await asyncio.sleep(DELETE_DELAY)
 
         try:
             await message.delete()
-            print(f"Удалено сообщение от @{username}")
+            print("Сообщение удалено")
         except Exception as e:
             print(f"Ошибка удаления: {e}")
 
